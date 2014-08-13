90ff85fe018eaa04e689180e976af355a498f00a
John Lotito <johnlotito@gmail.com>
2014-08-13 17:59:57 -0400

    Adding record video intent

--- samples/src/main/java/com/groupme/android/videokit/samples/MainActivity.java
+++ samples/src/main/java/com/groupme/android/videokit/samples/MainActivity.java
@@ -1,9 +1,15 @@
 package com.groupme.android.videokit.samples;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.groupme.android.videokit.VideoActivity;
 
 
 public class MainActivity extends Activity {
@@ -12,8 +18,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button recordVideo = (Button) findViewById(R.id.btn_record_video);
+        recordVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                intent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 30);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
